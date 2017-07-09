a6515efd7454e428d289fa75b5008fc67f90cc23
Sourabh Patil <soutya9@gmail.com>
2017-07-09 12:09:49 +0530

    module1-solution

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 (function() {
     'use strict';
-    angular.module('myFirstApp', [])
-        .controller('myFirstController', function($scope) {
-            $scope.name = 'Sourabh Patil';
-            $scope.Hello = function() {
-                return "Hello Fellas!"
+    angular.module('LunchCheck', [])
+        .controller('LunchCheckController', LunchCheckController);
+    LunchCheckController.$inject = ['$scope'];
+
+    function LunchCheckController($scope) {
+        console.log("in LunchCheckController function");
+        $scope.checkIt = function() {
+            console.log("in checkIt function");
+            var itemsList = 0;
+            $scope.message = "";
+            if (!$scope.items) {
+                $scope.message = "Enter data!";
+            } else {
+                var itemsListVal = $scope.items.split(',');
+                var i;
+                for (i = 0; i < itemsListVal.length; i++) {
+                    if (itemsListVal[i].trim().length > 0)
+                        itemsList++;
+                }
+                console.log(itemsList);
+                if (itemsList <= 3) $scope.message = "Enjoy!";
+                else $scope.message = "Too much!";
             }
-
-        });
-
-
+            $scope.itemsList = itemsList;
+        };
+    }
 })();
